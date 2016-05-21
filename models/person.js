da5ccd1a1df9a5cfdccb8826ac47f05903aaab6d
Satit Rianpit <siteslave@msn.com>
2016-05-21 23:38:46 +0700

    success WS2

--- models/person.js
+++ models/person.js
@@ -1,12 +1,34 @@
 var Q = require('q');
 
 module.exports = {
+  detail: function (db, hospcode, pid) {
+    var q = Q.defer();
+    var sql = `select p.HOSPCODE, p.PID, p.NAME, p.LNAME, p.BIRTH, p.SEX,
+      h.HOUSE, h.VILLAGE, chw.changwatname as CHANGWAT_NAME, cam.ampurname as AMPUR_NAME, ctb.tambonname as TAMBON_NAME
+      from person as p
+      inner join home as h on h.HOSPCODE=p.HOSPCODE and h.HID=p.HID
+      left join cchangwat as chw on chw.changwatcode=h.CHANGWAT
+      left join campur as cam on cam.ampurcode=h.AMPUR and cam.changwatcode=h.CHANGWAT
+      left join ctambon as ctb on ctb.tamboncodefull=concat(h.CHANGWAT, h.AMPUR, h.TAMBON)
+      where p.HOSPCODE=? and p.PID=?`;
+
+    db.raw(sql, [hospcode, pid])
+      .then(function (rows) {
+        q.resolve(rows[0]);
+      })
+      .catch(function (err) {
+        q.reject(err);
+      });
+
+    return q.promise;
+  },
   getPerson: function (db) {
     var q = Q.defer();
     db('person')
       .select()
+      .whereIn('TYPEAREA', ['1', '3'])
       .orderBy('NAME')
-      .limit(10)
+      .limit(20)
       .then(function (rows) {
         q.resolve(rows);
       })
@@ -24,7 +46,7 @@ module.exports = {
       .select()
       .where('NAME', 'like', _name)
       .orderBy('NAME')
-      .limit(10)
+      .limit(20)
       .then(function (rows) {
         q.resolve(rows);
       })
@@ -41,7 +63,7 @@ module.exports = {
       .select()
       .where('CID', cid)
       .orderBy('NAME')
-      .limit(10)
+      .limit(20)
       .then(function (rows) {
         q.resolve(rows);
       })
--- routes/index.js
+++ routes/index.js
@@ -18,6 +18,8 @@ router.post('/person/list', function (req, res, next) {
 
       rows.forEach(function (v) {
         var obj = {};
+        obj.hospcode = v.HOSPCODE;
+        obj.pid = v.PID;
         obj.sex = v.SEX;
         obj.fullname = v.NAME + ' ' + v.LNAME;
         obj.birth = moment(v.BIRTH).format('DD/MM/YYYY');
@@ -45,6 +47,8 @@ router.post('/person/search', function (req, res, next) {
 
         rows.forEach(function (v) {
           var obj = {};
+          obj.hospcode = v.HOSPCODE;
+          obj.pid = v.PID;
           obj.sex = v.SEX;
           obj.fullname = v.NAME + ' ' + v.LNAME;
           obj.birth = moment(v.BIRTH).format('DD/MM/YYYY');
@@ -66,6 +70,8 @@ router.post('/person/search', function (req, res, next) {
 
         rows.forEach(function (v) {
           var obj = {};
+          obj.hospcode = v.HOSPCODE;
+          obj.pid = v.PID;
           obj.sex = v.SEX;
           obj.fullname = v.NAME + ' ' + v.LNAME;
           obj.birth = moment(v.BIRTH).format('DD/MM/YYYY');
@@ -78,6 +84,23 @@ router.post('/person/search', function (req, res, next) {
         res.send({ ok: false, msg: err });
       });
   }
+});
+
+router.post('/person/detail', function (req, res, next) {
+  var db = req.db;
+  var hospcode = req.body.hospcode;
+  var pid = req.body.pid;
+
+  console.log(req.body);
+
+  Person.detail(db, hospcode, pid)
+    .then(function (rows) {
+      console.log(rows);
+      res.send({ ok: true, rows: rows });
+    }, function (err) {
+      res.send({ ok: false, msg: err });
+    });
+
 });
 
 module.exports = router;
